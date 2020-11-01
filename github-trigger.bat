79485728ab2bac94eddac64640ad86cf3a5ba7fa
Todor Trundev <trundev@gmail.com>
2020-11-01 16:08:18 +0200

    Result validation GitHub workflow
    Add a GitHub workflow file to run the ridges.py script on some DEM data.
    This includes complete preparation of the python environment, i.e. GDAL
    sources download and installation of the python module. Finally, the
    result is compared to a reference one, if such is available.
    
    The DEM data are downloaded from the http://usgs.gov STRM server ("push"
    event), or from an "input" URL ("workflow_dispatch" event). In the
    second case, other "input" parameters are also available, see
    github-trigger.bat.

---
+++ github-trigger.bat
@@ -0,0 +1,19 @@
+set REPO=%1
+if _%REPO% == _ set REPO=terrain_ridges
+
+set WORKFLOW=main.yml
+set WORKFLOW_REF=master
+set WORKFLOW_INPUTS=\"SRC_URL\": \"https://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/N39E021.hgt.zip\"
+set WORKFLOW_INPUTS=%WORKFLOW_INPUTS%, \"DST_EXT\": \".kmz\"
+set WORKFLOW_INPUTS=%WORKFLOW_INPUTS%, \"TOOL_OPTIONS\": \"\"
+set WORKFLOW_INPUTS=%WORKFLOW_INPUTS%, \"GDALWARP_OPTIONS\": \"-ts 600 600\"
+
+set OPT=
+set OPT=-H "Accept: application/vnd.github.v3+json" %OPT%
+::set OPT=--user trundev:<set GITHUB_TOKEN here> %OPT%
+
+::See https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
+@echo ============================================
+@echo Create a workflow dispatch event (HTTP POST)
+curl -X POST %OPT% https://api.github.com/repos/trundev/%REPO%/actions/workflows/%WORKFLOW%/dispatches -d "{\"ref\": \"%WORKFLOW_REF%\", \"inputs\": {%WORKFLOW_INPUTS%}}"
+@echo.
