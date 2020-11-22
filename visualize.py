66e53df0361f76f884b18dfd029e3eab12622f94
Todor Trundev <trundev@gmail.com>
2020-11-22 22:49:44 +0200

    Initial terrain and dir_arr visualization

---
+++ visualize.py
@@ -0,0 +1,319 @@
+"""3D DEM terrain visualization
+
+Requires: numpy matplotlib gdal pyproj
+"""
+import sys
+import numpy
+import matplotlib.pyplot as pyplot
+import matplotlib.widgets as widgets
+import gdal_utils
+import ridges
+
+NODATA_ALT = 1000.
+
+TERRAIN_FMT = dict(cmap='terrain', color='greenyellow', edgecolor='none', alpha=.5, label='_Terrain')
+NODATA_FMT = dict(color='red', marker='o', label='NoData')
+PENDINGS_FMT = dict(color='yellow', marker='o', label='Pendings')
+SEEDS_FMT = dict(color='orange', marker='x', label='Seeds')
+STOPS_FMT = dict(color='orange', marker='+', label='Stops')
+LEAFS_FMT = dict(color='lightgreen', marker='.', label='Leafs')
+DIR_ARR_FMT = dict(label='dir_arr', cmap='viridis', alpha=.5)
+DIR_ARR_CMAP_IDXS = 4
+NODES_FMT = dict(color='brown', marker='o', label='Nodes', alpha=.5)
+
+QUIVER_2D_FMT = dict(angles='xy', scale_units='xy', scale=1.2)
+
+USE_2D = True
+
+DIR_STYLE_LABELS = ['dir style 0', 'dir style 1']
+
+#
+# matplotlib Widget rectangles
+#
+AX_MARGIN = .02
+AX_BTN_WIDTH = .15
+AX_BTN_HEIGHT = .3
+
+def deflate_rect(rect, hor_margin=AX_MARGIN, vert_margin=AX_MARGIN):
+    """Deflate matplotlib rectangle [left, bottom, right, top]"""
+    rect[0] += hor_margin
+    rect[1] += vert_margin
+    rect[2] -= 2 * hor_margin
+    rect[3] -= 2 * vert_margin
+    return rect
+
+def set_aspect(ax, lonlatalt):
+    """Try to keep the lon/lat aspect equal"""
+    min_max = numpy.nanmin(lonlatalt, axis=(0,1)), numpy.nanmax(lonlatalt, axis=(0,1))
+    middle = (min_max[0] + min_max[1]) / 2
+    lonlan_max = (min_max[1] - min_max[0])[:2].max() / 3
+    # Reduce the altitude exaggeration
+    alt_max = (min_max[1] - min_max[0])[2]
+    # Set the matplotlib axis limits
+    ax.set_xlim(middle[0] - lonlan_max, middle[0] + lonlan_max)
+    ax.set_ylim(middle[1] - lonlan_max, middle[1] + lonlan_max)
+    ax.set_zlim(middle[2] - alt_max, middle[2] + alt_max)
+
+class collections:
+    """Maintain matplotlib collections"""
+    colls = {}
+    dir_style = 0
+
+    def __init__(self, ax, do_redraw, *do_redraw_args):
+        self.ax = ax
+        self.do_redraw = do_redraw
+        self.do_redraw_args = do_redraw_args
+
+    def get_collections(self):
+        """Obtain a list of collections"""
+        return self.colls.values()
+
+    def replace(self, key, new):
+        """Replace collection in a dictionary by keeping its visibility"""
+        old = self.colls.get(key)
+        if old is not None:
+            visible = old.get_visible()
+            old.remove()
+            new.set_visible(visible)
+        self.colls[key] = new
+
+    def redraw(self):
+        return self.do_redraw(self, *self.do_redraw_args)
+
+    def on_showhide(self, label):
+        if label in DIR_STYLE_LABELS:
+            idx = DIR_STYLE_LABELS.index(label)
+            self.dir_style ^= 1<<idx
+            self.redraw()
+        else:
+            # Show/hide collection
+            for coll in self.get_collections():
+                if coll.get_label() == label:
+                    coll.set_visible(not coll.get_visible())
+                    break
+        pyplot.draw()
+
+def do_redraw(colls, dem_band, dir_arr):
+    """Generate matplotlib collections"""
+    # Convert all points in the DEM buffer to 2D array of lon-lat-alt values (3D array)
+    shape = dem_band.get_elevation(True).shape
+    mgrid_xy = numpy.moveaxis(numpy.mgrid[:shape[0],:shape[1]], 0, -1)
+    lonlatalt = dem_band.xy2lonlatalt(mgrid_xy)
+
+    # When DEM contains no-data altitudes (NaN), plot_surface() may not render anything
+    nodata_mask = numpy.isnan(lonlatalt[...,2])
+    if nodata_mask.any():
+        lonlatalt[...,2][nodata_mask] = NODATA_ALT
+
+    if not USE_2D:
+        colls.replace('terrain', colls.ax.
+                plot_surface(*lonlatalt.T, **TERRAIN_FMT))
+
+    # NoData markers at 2/3-rds of altitude
+    nodata_pts = lonlatalt[nodata_mask]
+    if USE_2D:
+        nodata_pts = nodata_pts[...,:2]
+    else:
+        nodata_pts[...,2] = (2 * numpy.nanmax(lonlatalt[...,2]) + numpy.nanmin(lonlatalt[...,2])) / 3
+    colls.replace('nodata', colls.ax.
+            scatter(*nodata_pts.T, **NODATA_FMT))
+
+    if dir_arr is None:
+        return
+
+    #
+    # Visualize dir_arr
+    #
+    pts = lonlatalt
+    if USE_2D:
+        pts = pts[...,:2]
+
+    # Markers at "pending"
+    mask = dir_arr == ridges.NEIGHBOR_PENDING
+    colls.replace('pendings', colls.ax.
+            scatter(*pts[mask].T, **PENDINGS_FMT))
+
+    # Markers at "seed" and "stop"
+    mask = dir_arr == ridges.NEIGHBOR_SEED
+    colls.replace('seeds', colls.ax.
+            scatter(*pts[mask].T, **SEEDS_FMT))
+    mask = dir_arr == ridges.NEIGHBOR_STOP
+    colls.replace('stops', colls.ax.
+            scatter(*pts[mask].T, **STOPS_FMT))
+
+    # Helper array (sort of vector field)
+    mgrid_n_xy = ridges.neighbor_xy_safe(mgrid_xy, dir_arr)
+
+    # Markers at "leafs" (pixels w/o neighbor)
+    mask = numpy.ones(dir_arr.shape, dtype=bool)
+    gdal_utils.write_arr(mask, mgrid_n_xy, False)
+    colls.replace('leafs', colls.ax.
+            scatter(*pts[mask].T, **LEAFS_FMT))
+
+    # Invalid and seed points, points to them-self
+    vecs = dem_band.xy2lonlatalt(mgrid_n_xy) - lonlatalt
+    if USE_2D:
+        vecs = vecs[...,:2]
+    else:
+        # Suppress altitude as having problems with quiver() and different units
+        vecs[...,2] = 0
+
+    # Count the number of neighbors pointing to each pixel
+    n_num = numpy.zeros(dir_arr.shape, dtype=int)
+    for d in ridges.VALID_NEIGHBOR_DIRS:
+        n_xy = mgrid_n_xy[dir_arr == d]
+        n = numpy.zeros_like(n_num)
+        gdal_utils.write_arr(n, n_xy, 1)
+        n_num += n
+    # Add points where more than 2 neighbors pointing
+    mask = n_num > 1
+    print('Located %d node pixels, max %d forks'%(numpy.count_nonzero(mask), n_num.max()))
+    colls.replace('nodes', colls.ax.
+            scatter(*pts[mask].T, s=n_num[mask]**2, **NODES_FMT))
+
+    #
+    # Vectors along dir_arr
+    #
+    if USE_2D:
+        fmt = {**DIR_ARR_FMT, **QUIVER_2D_FMT}
+    else:
+        fmt = DIR_ARR_FMT
+
+    # Select how to color dir-vectors
+    show_mask = ~ridges.neighbor_is_invalid(dir_arr)
+    colors = numpy.zeros(dir_arr.shape, dtype=int)
+    cidx = 0
+    if colls.dir_style == 0:
+        # Colors expand staring at "seed" and "stop"
+        show_mask[...] = False
+        mask = (dir_arr == ridges.NEIGHBOR_SEED) | (dir_arr == ridges.NEIGHBOR_STOP)
+        mask = gdal_utils.read_arr(mask, mgrid_n_xy) ^ mask
+        while mask.any():
+            colors[mask] = cidx % DIR_ARR_CMAP_IDXS
+            show_mask[mask] = True
+            # Expand
+            mask = gdal_utils.read_arr(mask, mgrid_n_xy)
+            cidx += 1
+    elif colls.dir_style == 1:
+        # Colors contract staring at "leaf"
+        show_mask[...] = False
+        mask = numpy.ones(dir_arr.shape, dtype=bool)
+        gdal_utils.write_arr(mask, mgrid_n_xy, False)
+        while mask.any():
+            colors[mask] = cidx % DIR_ARR_CMAP_IDXS
+            show_mask[mask] = True
+            # Contract
+            m = mask
+            mask = numpy.zeros(dir_arr.shape, dtype=bool)
+            gdal_utils.write_arr(mask, mgrid_n_xy, m)
+            cidx += 1
+    elif colls.dir_style == 2:
+        # Colors based on elevation
+        colors = lonlatalt[...,2]
+    else:
+        # All in same color
+        pass
+
+    colors = colors[show_mask]
+    print('dir_arr sorted in %d steps'%cidx)
+
+    #TODO: Colors do not work in 3D-mode
+    if not USE_2D:
+        colors = None
+
+    # Show colored n_dir-s
+    quiver_list = colls.replace('dir_arr', colls.ax.
+            quiver(*pts[show_mask].T, *vecs[show_mask].T, colors, **fmt))
+
+def show_plot(dem_band, dir_arr, title=None):
+    """Display matplotlib window"""
+    fig = pyplot.figure()
+    ax = fig.add_axes(deflate_rect([0, 0, 1 - AX_BTN_WIDTH, 1], 2 * AX_MARGIN, AX_MARGIN),
+            projection=None if USE_2D else '3d')
+    if title is not None:
+        ax.set_title(title)
+
+    # Convert all points in the DEM buffer to 2D array of lon-lat-alt values (3D array)
+    shape = dem_band.get_elevation(True).shape
+    mgrid_xy = numpy.moveaxis(numpy.mgrid[:shape[0],:shape[1]], 0, -1)
+    lonlatalt = dem_band.xy2lonlatalt(mgrid_xy)
+
+    if USE_2D:
+        ax.set_aspect('equal')
+    else:
+        set_aspect(ax, lonlatalt)
+
+    colls = collections(ax, do_redraw, dem_band, dir_arr)
+    colls.redraw()
+
+    # Check boxes to show/hide individual elements
+    rax = fig.add_axes(deflate_rect([1 - AX_BTN_WIDTH, 1 - AX_BTN_HEIGHT, AX_BTN_WIDTH, AX_BTN_HEIGHT],
+            AX_MARGIN / 2, AX_MARGIN / 2))
+    labels = [coll.get_label() for coll in colls.get_collections()]
+    visibility = [coll.get_visible() for coll in colls.get_collections()]
+    labels += DIR_STYLE_LABELS
+    visibility += [False] * len(DIR_STYLE_LABELS)
+    check = widgets.CheckButtons(rax, labels, visibility)
+    check.on_clicked(colls.on_showhide)
+
+    ax.legend()
+    pyplot.show()
+    return 0
+
+def main(argv):
+    """Main entry"""
+    dem_filename = None
+    dir_arr = None
+    while argv:
+        if argv[0][0] == '-':
+            if argv[0] == '-h':
+                return print_help()
+            elif argv[0] == '--dir_arr':
+                argv = argv[1:]
+                dir_arr = argv[0]
+            else:
+                return print_help('Unsupported option "%s"'%argv[0])
+        else:
+            if dem_filename is None:
+                dem_filename = argv[0]
+            else:
+                return print_help('Unexpected argument "%s"'%argv[0])
+
+        argv = argv[1:]
+
+    if dem_filename is None:
+        return print_help('Missing file-name')
+
+    # Load DEM
+    dem_band = gdal_utils.dem_open(dem_filename)
+    if dem_band is None:
+        return print_help('Unable to open "%s"'%dem_filename)
+    dem_band.load()
+
+    # Load 'dir_arr'
+    if dir_arr is not None:
+        dir_arr = numpy.load(dir_arr)
+        if (dir_arr.shape != dem_band.get_elevation(True).shape):
+            return print_help('DEM and "dir_arr" shape mismatch"%s"'%dem_filename)
+
+    numpy.set_printoptions(suppress=True, precision=6)
+    print('%s %d x %d:'%(dem_filename, *dem_band.dem_buf.shape))
+    print(dem_band.xform)
+
+    # Visualize terrain/ridges
+    res = show_plot(dem_band, dir_arr, dem_filename)
+    return res
+
+def print_help(err_msg=None):
+    if err_msg:
+        print('Error:', err_msg, file=sys.stderr)
+    print('Usage:', sys.argv[0], '[<options>] <dem_filename>')
+    print('\tOptions:')
+    print('\t-h\t- This screen')
+    print('\t--dir_arr <dir_arr-npy>\t- "dir_arr" (n_dir) npy file')
+    return 0 if err_msg is None else 255
+
+if __name__ == '__main__':
+    ret = main(sys.argv[1:])
+    if ret:
+        exit(ret)
