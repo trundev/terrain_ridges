b925e3e0056f4e14680b1488a6723f8059311ff7
Todor Trundev <trundev@gmail.com>
2025-04-19 09:43:14 +0300

    Add dash-cytoscape visualization tool
    Includes HTML dash server, hosting dash-cytoscape and client module to
    prepare and submit graph data.

---
+++ tools/visualize_dash-server.py
@@ -0,0 +1,165 @@
+"""Dash server for visualization of internal graph structures, uses dash-cytoscape
+
+See: https://dash.plotly.com/ and https://js.cytoscape.org/
+"""
+import sys, os
+import uuid
+import json
+import dash
+import flask
+import dash_cytoscape as cyto
+
+# Title/elements to show at startup
+DEFAULT_CYTO_TITLE = dash.html.I('Use visualize_dash.visualize_graph() to upload the graph')
+DEFAULT_CYTO_ELEMENTS =[
+        dict(data=dict(id='x', label='wait...'),
+             classes='invalid',
+            )]
+
+CYTOSCAPE_LAYOUTS = ['preset', 'random', 'grid', 'circle', 'concentric', 'breadthfirst', 'cose']
+CYTOSCAPE_LAYOUT = dict(name='preset')
+DASH_STYLE = dict(width='100%', height='600px')
+# Style sheet (https://js.cytoscape.org/#style)
+DASH_STYLE_SHEET = [
+    dict(selector='node', style={
+            'label': 'data(label)',
+            }),
+    dict(selector='edge', style={
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier',
+            'label': 'data(label)',
+            }),
+    # Classes
+    dict(selector='edge.passive', style={
+            'line-style': 'dashed',
+            #'line-dash-pattern': [3,10],
+            'opacity': '.7',
+        }),
+    dict(selector='edge.ghost', style={
+            'line-style': 'dotted',
+            'opacity': '.5',
+        }),
+    dict(selector='node.leaf', style={
+            'background-color': 'green',
+            'border-color': 'green',
+            'shape': 'triangle',
+        }),
+    dict(selector='node.root', style={
+            'background-color': 'cyan',
+            'border-color': 'cyan',
+            'shape': 'star',
+        }),
+    dict(selector='node.loop', style={
+            'background-color': 'blue',
+            'border-color': 'blue',
+            'shape': 'square',
+        }),
+    dict(selector='node.invalid', style={
+            'background-color': 'red',
+            'shape': 'octagon',
+        }),
+    dict(selector='node.parent', style={
+            'shape': 'square',  # Empty parent-node only
+        }),
+    ]
+
+#
+# Dash server related
+#
+def create_cytoscape(app: dash.Dash) -> tuple[
+        dash.development.base_component.Component,
+        dash.development.base_component.Component,
+        cyto.Cytoscape]:
+    """Create the Cytoscape object"""
+    #
+    # Cytoscape object and a layout dropdown
+    #
+    title_comp = dash.html.Label(DEFAULT_CYTO_TITLE)
+    cyto_comp = cyto.Cytoscape(elements=DEFAULT_CYTO_ELEMENTS,
+                id=str(uuid.uuid4()),
+                layout=CYTOSCAPE_LAYOUT,
+                style=DASH_STYLE,
+                stylesheet=DASH_STYLE_SHEET)
+    dropdown = dash.dcc.Dropdown(
+            id=cyto_comp.id +'-dropdown',
+            value=cyto_comp.layout.get('name'),
+            clearable=False,
+            options=[{'label': name, 'value': name} for name in CYTOSCAPE_LAYOUTS]
+        )
+    @app.callback(dash.Output(cyto_comp.id, 'layout'),
+                dash.Input(dropdown.id, 'value'))
+    def update_layout(layout):
+        return {'name': layout}
+
+    #
+    # Combine into Fieldset
+    #
+    return title_comp, cyto_comp, dash.html.Fieldset([title_comp, cyto_comp, dropdown])
+
+def run_dash() -> None:
+    """Start the Dash server"""
+    server = flask.Flask(__name__)
+    app = dash.Dash(__name__, server=server)
+    #
+    # Cytoscape object
+    #
+    title_comp, cyto_comp, cyto_container = create_cytoscape(app)
+
+    #
+    # Handle POST/GET requests
+    #
+    @server.route('/cytoscape', methods=['POST', 'GET'])
+    def handle_request() -> str:
+        """Flask server """
+        request = flask.request
+        if request.method == 'POST':
+            data = request.get_json()  # For JSON data
+        elif request.method == 'GET':
+            data = request.args.get('elements')
+            if data is None:
+                return f'GET request with no elements'
+            data = json.loads(data)
+        else:
+            return f'Request method {request.method} not supported'
+
+        title = request.args.get('title')
+        data_str = f'{data[:10]}...' if len(data) > 10 else data
+        print(f'Received POST title {title}, data:')
+        print(f'  {data_str}')
+
+        title_comp.children = title
+        cyto_comp.elements = data
+        return f'Received {request.method}, title {title}'
+
+    #
+    # Main layout
+    #
+    app.layout = [
+            dash.html.H2(os.path.basename(__file__)),
+            dash.html.Button('Refresh', id='refresh_layout'),
+            cyto_container,
+        ]
+
+    #
+    # Handle "Refresh" button
+    #
+    @app.callback(dash.Output(cyto_comp.id, 'elements'),
+                  dash.Input('refresh_layout', 'n_clicks'),
+                  dash.State(cyto_comp.id, 'elements'),
+                  prevent_initial_call=True)
+    def refresh(n_clicks, elements):
+        if n_clicks is None:
+            raise dash.exceptions.PreventUpdate
+        return elements
+
+    return app.run(debug=True, use_reloader=False)
+
+#
+# Main entry
+#
+def main(argv) -> None:
+    """Standalone execution (TODO: import data from file)"""
+    run_dash()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ tools/visualize_dash.py
@@ -0,0 +1,190 @@
+"""Visualization of internal graph structures (for development purposes)
+
+See: https://dash.plotly.com/ and https://js.cytoscape.org/
+
+[Hint] visualization one-liners:
+- Run server
+  >>> py tools/visualize_graph-server.py
+- Visualize graph
+  >>> from tools import visualize_dash as vis
+  >>> vis.visualize_graph(graph_edges, passive_edges=True, node_class=True, node_parent=True)
+"""
+import sys
+import itertools
+import uuid
+import urllib.parse
+import numpy as np
+import numpy.typing as npt
+import requests
+from terrain_ridges import topo_graph
+from terrain_ridges.topo_graph import T_Graph, T_IndexArray, T_MaskArray
+
+
+# Obtain node position (layout - preset) by scaling node ID
+MODEL_POSITION_STEP = 120
+# Parent node ID to assign to invalid nodes
+INVALID_PARENT_ID = 'invalid'
+
+def vals_to_colorscale(vals: T_IndexArray) -> npt.NDArray[np.str_]:
+    import plotly.colors as clrs
+    idx = (vals - vals.min()) / (vals.max() - vals.min()) * (len(clrs.sequential.Viridis) - 1)
+    return np.asarray(clrs.sequential.Viridis)[idx.round().astype(int)]
+
+#
+# Graph/edge visualization
+#
+def graph_to_elements(graph_edges: T_Graph, edge_mask: T_MaskArray|bool=True, *,
+                      passive_edges: T_MaskArray|bool=False,
+                      node_class: npt.NDArray[np.str_]|None=None,
+                      node_parent: T_IndexArray|None=None) -> list[dict[str, str|int]]:
+    """Create cytoscape elements list from graph (list of edges)
+
+    Parameters
+    ----------
+    graph_edges : (node-coord, 2, edge-indices) ndarray of int
+        Graph to visualize
+    edge_mask : (edge-indices) ndarray of bool [optional]
+        Mask of graph-edges to visualize, for correct edge index text-labels
+    passive_edges : (edge-indices) ndarray of bool [optional]
+        Mask of graph-edges to visualize using 'edge.passive' selector,
+        where it overlaps `edge_mask`, 'edge.ghost' is used
+    node_class : (node-index) ndarray of str [optional]
+        Add class selector to nodes, to indicate node-type, currently available:
+        'leaf', 'root', 'loop' and 'invalid'
+    node_parent : (node-index) ndarray [optional]
+        Attach nodes to "compound parent" nodes, see:
+        https://js.cytoscape.org/#notation/compound-nodes
+    """
+    edge_mask = np.broadcast_to(edge_mask, graph_edges.shape[2:])
+    passive_edges = np.broadcast_to(passive_edges, graph_edges.shape[2:])
+    # Color nodes based on parent
+    node_color = None
+    if node_parent is not None:
+        node_color = vals_to_colorscale(node_parent)
+
+    elements = []
+    # Add nodes, structure:
+    # - data:
+    #   - id: <node-id>
+    #   - label: <node label>
+    #   - parent: <node-id>
+    #   classes: [<class-id>]
+    #   style: {background-color: <color>}
+    for node_id in np.unique(graph_edges.reshape(graph_edges.shape[:1] + (-1,)), axis=1).T:
+        data = dict(
+            id=str(node_id),
+            label=f'node: {node_id}')
+        element = dict(data=data)
+        # Parent node, use INVALID_PARENT_ID for invalid nodes
+        if node_parent is not None:
+            data |= dict(parent=str(node_parent[*node_id])
+                                    if (node_id >= 0).all() else INVALID_PARENT_ID)
+        # Regular vs. invalid node
+        if (node_id >= 0).all():
+            # Node type class
+            if node_class is not None:
+                element |= dict(classes=node_class.item(*node_id))
+            # Assign colors to nodes
+            if node_color is not None:
+                element |= dict(style={'background-color': node_color.item(*node_id)})
+        else:
+            element |= dict(classes='invalid')
+        # Preset position (avoid numpy types as they are not JSON serializable)
+        if node_id.size >= 2:
+            element |= dict(position={k: v.item(0) for k, v in
+                                      zip('xy', node_id * MODEL_POSITION_STEP)})
+        elements.append(element)
+    # Add parent-nodes, if necessary the one for invalid nodes
+    if node_parent is not None:
+        parent_ids = np.unique(node_parent[node_parent >= 0])
+        if (graph_edges < 0).any():
+            parent_ids = itertools.chain(parent_ids, [INVALID_PARENT_ID])
+        for node_id in parent_ids:
+            elements.append(dict(
+                    data=dict(
+                        id=str(node_id),
+                        label=f'parent: {node_id}'),
+                    classes='parent'))
+
+    # Add edges, structure:
+    # - data:
+    #   - source: <node-id>
+    #   - target: <node-id>
+    #   - label: <edge label>
+    #   classes: [<class-id>]
+    for idx in np.asarray(np.nonzero(edge_mask | passive_edges)).T:
+        # Edge class is selected from `edge_mask` and `passive_edges`
+        classes = ''
+        if passive_edges.item(*idx):
+            classes=('ghost', 'passive')[edge_mask.item(*idx)]
+        elements.append(dict(
+                data=dict(
+                    source=str(graph_edges[:, 0, *idx]),
+                    target=str(graph_edges[:, 1, *idx]),
+                    label=f'edge: {np.asarray(idx)}'),
+                classes=classes))
+    return elements
+
+#
+# Client side code
+#
+GRAPH_ID = str(uuid.uuid4())
+SERVER_URL = 'http://127.0.0.1:8050/cytoscape?'
+
+def visualize_graph(graph_edges: T_Graph, *, id: str=GRAPH_ID, **kwargs) -> str:
+    """Convert graph to cytoscape format and post it to the server
+
+    See graph_to_elements()
+    """
+    # Auto-generate `passive_edges`, `node_class`, etc.
+    if kwargs.get('passive_edges') is True:
+        kwargs['passive_edges'] = ~topo_graph.valid_node_edges(graph_edges)
+    if kwargs.get('node_class') is True:
+        root_nodes = topo_graph.isolate_graph_sinks(graph_edges)
+        leaf_nodes = topo_graph.isolate_graph_sinks(graph_edges[:, ::-1])
+        loop_nodes = topo_graph.propagate_mask(graph_edges, True)
+        loop_nodes = topo_graph.propagate_mask(graph_edges[:, ::-1], loop_nodes)
+        kwargs['node_class'] = np.where(loop_nodes, 'loop',
+                np.where(root_nodes, 'root', np.where(leaf_nodes, 'leaf', '')))
+    if kwargs.get('node_parent') is True:
+        parent_ids = topo_graph.isolate_subgraphs(graph_edges)
+        kwargs['node_parent'] = parent_ids
+
+    cyto_elements = graph_to_elements(graph_edges, **kwargs)
+    url = SERVER_URL + urllib.parse.urlencode(dict(id=id, title=f'Graph - edges: {graph_edges.shape[2:]}'))
+    res = requests.post(url, json=cyto_elements)
+    return res.text
+
+#
+# Main entry
+#
+def main(argv) -> int:
+    """Standalone execution (TODO: import data from file)"""
+    # Show some demo graph
+    grid_edges = np.array((
+            #
+            # Straight line from leaf (0,0) to root (0,2)
+            [(0,0), (0,1)], [(0,1), (0,2)],
+            #
+            # Loop
+            [(1,0), (1,1)], [(1,1), (1,0)],
+            # Loop toward root
+            [(1,1), (1,2)], [(1,2), (1,3)], # root (1,3)
+            # Toward the loop, leaf (1,5)
+            [(1,5), (1,4)], [(1,4), (1,0)],
+            #
+            # Invalid source-node to root (2,1)
+            [(2,-1), (2,0)], [(2,0), (2,1)],
+            #
+            # Invalid target-node from leaf (3,0)
+            [(3,0), (3,1)], [(3,1), (3,-1)],
+            #
+            # Invalid source and target-nodes
+            [(-1,-1), (4,-1)],
+        ), dtype=int).T
+    res = visualize_graph(grid_edges, passive_edges=True, node_class=True, node_parent=True)
+    print(res)
+    return 0
+
+if __name__ == '__main__':
+    exit(main(sys.argv[1:]))
